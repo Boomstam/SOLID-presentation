ca520b623bc6e438c79cb56a71061805f6feaa28
Menno Buggenhout <mennobuggenhout@gmail.com>
2021-10-16 17:19:12 +0200

    Introduction

---
+++ src/Introduction/Introduction.java
@@ -0,0 +1,45 @@
+/*
+
+
+SOLID Design principles
+
+
+=> Introduced by 'Uncle Bob'; the nickname of software engineer and instructor Robert C. Martin in 2000.
+Has many interesting (free) videos/podcast available, bv on SOLID: https://www.youtube.com/watch?v=zHiWqnTWsn4.
+He is also the person that coined the term 'clean code'.
+
+
+=> Set of ideas/principles/design patterns that help to create maintainable, 'clean' code.
+
+
+- - - - - - -
+
+The Single-responsibility principle:
+"There should never be more than one reason for a class to change."
+In other words, every class should have only one responsibility.
+
+The Open–closed principle:
+"Software entities ... should be open for extension, but closed for modification."
+
+The Liskov substitution principle:
+"Functions that use pointers or references to base classes must be able to use objects of derived classes without knowing it."
+
+The Interface segregation principle:
+"Many client-specific interfaces are better than one general-purpose interface."
+
+The Dependency inversion principle:
+"Depend upon abstractions, not concretions."
+
+- - - - - - -
+
+
+=> Is used in practice as an integral part of for example in agile programming, adaptive software development, ...
+https://en.wikipedia.org/wiki/Agile_software_development
+https://en.wikipedia.org/wiki/Adaptive_software_development
+
+
+=> Also useful as an way to find 'code smells', any violation of the principles is in itself a code smell.
+https://en.wikipedia.org/wiki/Code_smell
+
+
+*/
