63d9451e75496bba1f26d94938286e6879c617b0
Menno Buggenhout <mennobuggenhout@gmail.com>
2021-10-16 17:30:16 +0200

    working on Single Responsibility

--- src/SingleResponsibility/TooMuchResponsibilities.java
+++ src/SingleResponsibility/TooMuchResponsibilities.java
@@ -1,4 +1,17 @@
 package SingleResponsibility;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class TooMuchResponsibilities {
+
+    public static void main(String[] args) {
+        printDate();
+    }
+
+    private static void printDate(){
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(dtf.format(now));
+    }
 }
